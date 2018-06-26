4cd921e455681face124a2abcad688dcee5684f8
Vinicius Cainelli <vicainelli@gmail.com>
2018-06-26 11:11:32 -0300

    Update vscode.js

--- vscode.js
+++ vscode.js
@@ -1,17 +1,14 @@
-// Files
 {
   "files.autoSave": "onFocusChange",
 
   "editor.fontFamily": "Operator Mono, Menlo, Monaco, 'Courier New', monospace",
   "editor.fontSize": 12,
-  // "editor.lineHeight": 40,
   "editor.letterSpacing": 0.5,
   "workbench.editor.tabSizing": "shrink",
   "html.format.enable": true,
   "html.format.preserveNewLines": true,
   "files.trimTrailingWhitespace": true,
   "editor.tabSize": 2,
-  // this isn't really underline but we hack it to be a thicker cursor
   "editor.cursorStyle": "line",
   "editor.cursorWidth": 3,
   "editor.wordWrap": "on",
@@ -22,14 +19,13 @@
   "editor.minimap.enabled": false,
   "workbench.activityBar.visible": true,
   "workbench.sideBar.location": "left",
-  // "gitlens.blame.line.enabled": false,
+  "gitlens.blame.line.enabled": false,
   "editor.renderWhitespace": "none",
   "editor.rulers": [
     80,
     100,
     120
   ],
-  // "editor.minimap.showSlider": "always",
   "workbench.startupEditor": "newUntitledFile",
   "extensions.ignoreRecommendations": false,
   "files.exclude": {
@@ -45,31 +41,14 @@
   "emmet.includeLanguages": {
     "javascript": "javascriptreact"
   },
-  // show snippets before any other auto completion
   "editor.snippetSuggestions": "top",
-  // this lets you quickly open multiple items in a row and not have the previous ones closed on you
   "workbench.editor.enablePreviewFromQuickOpen": false,
   "window.title": "${dirty} ${activeEditorMedium}${separator}${rootName}",
   "sublimeTextKeymap.promptV3Features": true,
-  // These are all my auto-save configs
-  "editor.formatOnSave": true,
-  // turn it off for JS
   "[javascript]": {
     "editor.formatOnSave": false
   },
   "eslint.autoFixOnSave": true,
   "eslint.alwaysShowStatus": true,
-  "editor.renderIndentGuides": false,
-  "sync.gist": "dbfa0f52fd61732a4c088bbd7ebfc272",
-  "sync.host": "",
-  "sync.pathPrefix": "",
-  "sync.quietSync": false,
-  "sync.askGistName": false,
-  "sync.removeExtensions": true,
-  "sync.syncExtensions": true,
-  "sync.autoDownload": false,
-  "sync.autoUpload": false,
-  "sync.lastUpload": "2018-06-26T14:05:00.461Z",
-  "sync.lastDownload": "",
-  "sync.forceDownload": false
+  "editor.renderIndentGuides": false
 }
